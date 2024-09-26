760d6356fa656a6a44451f58f9d193e1f46e8020
markilha@gmail.com <markilha@gmail.com>
2024-09-26 16:48:20 -0300

    fix: removido conexao banco oracle

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DbUsersModule } from './database/db_mysql/db.users.module';
-import { S001RequisicaoModule } from './requisicao/s001_requisicao.module';
-import { RmModule } from './rm/rm.module';
-import { DbOraModule } from './database/db_oracle/db.ora.module';
 import { UsureqModule } from './usureq/usureq.module';
-import { PfuncaoModule } from './pfuncao/pfuncao.module';
-import { PsecaoModule } from './psecao/psecao.module';
-import { ReqviagemModule } from './reqviagem/reqviagem.module';
-
 import { ReqnumerarioModule } from './reqnumerario/reqnumerario.module';
-import { SaqueModule } from './saque/saque.module';
 import { UfespModule } from './ufesp/ufesp.module';
 import { PcargoModule } from './pcargo/pcargo.module';
 import { FuncsalarioModule } from './funcsalario/funcsalario.module';
@@ -19,23 +11,14 @@ import { FuncsalarioModule } from './funcsalario/funcsalario.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    DbOraModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), 
     DbUsersModule,
-    AuthModule,
-    S001RequisicaoModule,
-    RmModule,
-    UsureqModule,
-    PfuncaoModule,
-    PsecaoModule,
-    ReqviagemModule,
-    ReqviagemModule,
-    ReqnumerarioModule,
-    SaqueModule,
+    AuthModule,   
+    UsureqModule,  
+    ReqnumerarioModule,  
     UfespModule,
     PcargoModule,
-    FuncsalarioModule
-    
+    FuncsalarioModule    
   ],
   controllers: [],
   providers: [],
