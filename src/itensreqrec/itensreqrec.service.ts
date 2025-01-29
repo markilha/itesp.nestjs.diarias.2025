a1c66560fa78f74824ca80a5770b873d1271460b
markilha@gmail.com <markilha@gmail.com>
2025-01-29 06:24:49 -0300

    feat: atualizar o tratamento de recursos do item e melhorar as mensagens de erro em itensreqrecService

--- src/itensreqrec/itensreqrec.service.ts
+++ src/itensreqrec/itensreqrec.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { itensreqrecEntity } from '../database/db_oracle/entities/itensreqrec.entity';
 import { Repository } from 'typeorm';
-import { paramsItemRecurso } from './itensreq.Dto';
+import { itemRecursoDto, paramsItemRecurso } from './itensreq.Dto';
 import {
   SelecionaItenFunc,
   SelecionaItensFunc,
@@ -20,13 +20,8 @@ export class itensreqrecService {
     try {
       const item = await this.itensreqrecRepository.create(data);
       return await this.itensreqrecRepository.save(item);
-      
-    } catch (error) {     
-      throw new HttpException(
-        "Ocorreu erro ao criar item recurso",
-        HttpStatus.NOT_FOUND,
-      );
-      
+    } catch (error) {
+      throw new HttpException('Ocorreu erro ao criar item recurso', HttpStatus.NOT_FOUND);
     }
   }
 
@@ -48,14 +43,14 @@ export class itensreqrecService {
         LEFT JOIN FINANCEIRO.S009_TIPODESP d ON d.TDE_ID_CODIGO = i.TDE_ID_CODIGO
         WHERE ITE_ID_CODIGO = :ITE_ID_CODIGO`,
         [ITE_ID_CODIGO],
-      );    
-  
+      );
+
       if (!item || item.length === 0) {
         throw new HttpException(
           `Item com código: ${ITE_ID_CODIGO} não encontrado`,
           HttpStatus.NOT_FOUND,
         );
-      }  
+      }
       return item[0];
     } catch (error) {
       throw new HttpException(
@@ -64,7 +59,6 @@ export class itensreqrecService {
       );
     }
   }
-  
 
   async selecionaItensRecurso(params: paramsItemRecurso): Promise<any> {
     try {
@@ -100,31 +94,60 @@ export class itensreqrecService {
     }
   }
 
-  async update(newItem: itensreqrecEntity): Promise<itensreqrecEntity> {
+  async update(newItem: any): Promise<itemRecursoDto> {
+    const { ITE_ID_CODIGO } = newItem;
+
+    const newitensRecurso = new itemRecursoDto({
+      ITE_ID_CODIGO: newItem.ITE_ID_CODIGO,
+      RRE_ID_CODIGO: newItem.RRE_ID_CODIGO,
+      DIR_ID_CODIGO: newItem.DIR_ID_CODIGO,
+      ORI_ID_CODIGO: newItem.ORI_ID_CODIGO,
+      FPA_ID_CODIGO: newItem.FPA_ID_CODIGO,
+      STS_ID_CODIGO: newItem.STS_ID_CODIGO,
+      CHAPA: newItem.CHAPA,
+      CODCOLIGADA: newItem.CODCOLIGADA,
+      IRR_VALOR_SOL: newItem.IRR_VALOR_SOL,
+      IRR_DATA_SOL: newItem.IRR_DATA_SOL,
+      IRR_VALOR_CONC: newItem.IRR_VALOR_CONC,
+      IRR_DATA_CONC: newItem.IRR_DATA_CONC,
+      IRR_VALOR_PREST: newItem.IRR_VALOR_PREST,
+      IRR_DATA_PREST: newItem.IRR_DATA_PREST,
+      IRR_JUSTIFICA: newItem.IRR_JUSTIFICA,
+      IRR_RECURSO: newItem.IRR_RECURSO,
+      IRR_VALOR_AUT: newItem.IRR_VALOR_AUT,
+      IRR_VLRECEBIDO: newItem.IRR_VLRECEBIDO,
+      IRR_VLDEVOLUCAO: newItem.IRR_VLDEVOLUCAO,
+      IRR_VLSAQUE: newItem.IRR_VLSAQUE,
+      IRR_VLTRANSFERIDO: newItem.IRR_VLTRANSFERIDO,
+      IRR_SALDO: newItem.IRR_SALDO,
+      IRR_COMPLEMENTO: newItem.IRR_COMPLEMENTO,
+      IRR_VLREGIONAL: newItem.IRR_VLREGIONAL,
+      TDE_ID_CODIGO: newItem.TDE_ID_CODIGO,
+    });
     try {
-      // Busca o item existente no banco de dados
-      const item = await this.findOne(newItem.ITE_ID_CODIGO);
+      // Busca o item existente
+      const item = await this.findOne(ITE_ID_CODIGO);
       if (!item) {
         throw new HttpException(
-          `Item com código: ${newItem.ITE_ID_CODIGO} não encontrado`,
+          `Item com código: ${ITE_ID_CODIGO} não encontrado`,
           HttpStatus.NOT_FOUND,
         );
-      }  
-      const updatedItem = this.itensreqrecRepository.merge(item, newItem); 
+      }
+
       await this.itensreqrecRepository
         .createQueryBuilder()
-        .update("FINANCEIRO.S009_ITENSREQREC")
-        .set(updatedItem)
-        .where("ITE_ID_CODIGO = :ITE_ID_CODIGO", { ITE_ID_CODIGO: newItem.ITE_ID_CODIGO })
+        .update('FINANCEIRO.S009_ITENSREQREC')
+        .set(newitensRecurso)
+        .where('ITE_ID_CODIGO = :ITE_ID_CODIGO', { ITE_ID_CODIGO })
         .execute();
-      return updatedItem;
+
+      return newitensRecurso;
     } catch (error) {
-      console.error(error);
+      console.error(`Erro ao atualizar o item com código: ${ITE_ID_CODIGO}`, error);
       throw new HttpException(
-        `Erro ao atualizar o item com código: ${newItem.ITE_ID_CODIGO}. ${error.message}`,
+        `Erro ao atualizar o item com código: ${ITE_ID_CODIGO}. ${error.message}`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-  }  
-
+  }
 }
