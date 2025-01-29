54f8e2cdfdd2cbb21c28a6e89b858d4e7785e44b
markilha@gmail.com <markilha@gmail.com>
2025-01-29 10:24:22 -0300

    feat: implementar paginação e autorização no método findAll de docpcontasnumService

--- src/docpcontasnum/docpcontasnum.service.ts
+++ src/docpcontasnum/docpcontasnum.service.ts
@@ -3,7 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { docpcontasnumEntity } from '../database/db_oracle/entities/docpcontasnum.entity';
 import { FindOptionsWhere, Repository } from 'typeorm';
 import { FindAllParams, returnData } from './docpcontasnumDto';
-import { http } from 'winston';
+
+import { permissaoFindAll } from 'src/util/permissao/permissao';
+import { AuthUserDto } from 'src/auth/use.auth.Dto';
 
 @Injectable()
 export class docpcontasnumService {
@@ -12,8 +14,13 @@ export class docpcontasnumService {
     private docpcontasnumRepository: Repository<docpcontasnumEntity>,
   ) {}
 
-  async findAll(params: FindAllParams): Promise<returnData> {
+  async findAll(params: FindAllParams, user: AuthUserDto): Promise<returnData> {
     try {
+      const pageNumber = params.page ?? 1;
+      const pageSize = params.limit ?? 500;
+      const startRow = (pageNumber - 1) * pageSize + 1;
+      const endRow = pageNumber * pageSize;
+
       const searchParams: FindOptionsWhere<docpcontasnumEntity> = {};
       if (params.SQE_ID_CODIGO) {
         searchParams['SQE_ID_CODIGO'] = params.SQE_ID_CODIGO;
@@ -24,33 +31,77 @@ export class docpcontasnumService {
       if (params.REG_ID_CODIGO) {
         searchParams['REG_ID_CODIGO'] = params.REG_ID_CODIGO;
       }
-      if (params.CHAPA) {
-        searchParams['CHAPA'] = params.CHAPA;
-      }
 
-      let dados: docpcontasnumEntity[] = [];
+      const queryBuilder = this.docpcontasnumRepository
+        .createQueryBuilder('r')
+        .select([
+          'r.NDO_ID_CODIGO as NDO_ID_CODIGO',
+          'r.SQE_ID_CODIGO as SQE_ID_CODIGO',
+          'r.PCO_ID_CODIGO as PCO_ID_CODIGO',
+          'r.PES_ID_CODIGO as PES_ID_CODIGO',
+          'r.PES_PESSOA as PES_PESSOA',
+          'r.NDO_ID_NUMERO as NDO_ID_NUMERO',
+          'r.NDO_DATA as NDO_DATA',
+          'r.NDO_DTENTREGA as NDO_DTENTREGA',
+          'r.NDO_OPERADOR as NDO_OPERADOR',
+          'r.STS_ID_CODIGO as STS_ID_CODIGO',
+          'r.NDO_SERIE as NDO_SERIE',
+          'r.NDO_TITULO as NDO_TITULO',
+          'r.PES_NOME as PES_NOME',
+          'r.CHAPA as CHAPA',
+          'r.NOME as NOME',
+          'r.CODIGO as CODIGO',
+          'r.DESCRICAO as DESCRICAO',
+          'r.VALORTOTAL as VALORTOTAL',
+          'r.SQE_DTPREST as SQE_DTPREST',
+          'r.TDE_DESCRICAO as TDE_DESCRICAO',
+          'r.TDE_ID_CODIGO as TDE_ID_CODIGO',
+          'r.SQE_VLSAQUE as SQE_VLSAQUE',
+          'r.SQE_VLPREST as SQE_VLPREST',
+          'r.SQE_DTSAQUE as SQE_DTSAQUE',
+          'r.PCO_TIPO as PCO_TIPO',
+          'r.PCO_TOTDOC as PCO_TOTDOC',
+          'r.PRA_ID_CODIGO as PRA_ID_CODIGO',
+          'r.REQ_ID_CODIGO as REQ_ID_CODIGO',
+          'r.SQE_EFETIVO as SQE_EFETIVO',
+          'r.REG_ID_CODIGO as REG_ID_CODIGO',
+          'fs.CODSECAO as CODSECAO',
+        ])
+        .leftJoin('PFUNC', 'fs', 'r.CHAPA = fs.CHAPA')
+        .where(searchParams);
 
-      if (params.page && params.limit) {
-        const page = params.page;
-        const limit = params.limit;
-        const skip = (page - 1) * limit;
+      const per = permissaoFindAll(user.permissao);
 
-        dados = await this.docpcontasnumRepository.find({
-          where: searchParams,
-          skip,
-          take: limit,
+      if (per) {
+        queryBuilder.andWhere('fs.CODSECAO LIKE :codsecao', {
+          codsecao: `${user.codsecao.substring(0, per)}%`,
         });
+      } else if (params.CHAPA) {
+        queryBuilder.andWhere('r.CHAPA = :chapa', { chapa: user.chapa });
       }
-
-      dados = await this.docpcontasnumRepository.find({
-        where: searchParams,
-      });
+     
+      // Paginação
+      const paginatedQuery = `
+      WITH base_query AS (${queryBuilder.getQuery()}),
+      count_query AS (SELECT COUNT(*) as total_count FROM base_query),
+      paginated_data AS (SELECT a.*, ROWNUM rnum 
+        FROM (SELECT * FROM base_query) a 
+        WHERE ROWNUM <= ${endRow}
+      )
+      SELECT pd.*, cq.total_count
+      FROM paginated_data pd
+      CROSS JOIN count_query cq
+      WHERE pd.rnum >= ${startRow}`;
+      
+      const parameters = Object.values(queryBuilder.getParameters());
+      const result = await this.docpcontasnumRepository.query(paginatedQuery, parameters);
 
       return {
-        data: dados,
-        total: dados.length,
+        data: result,
+        total: result?.length > 0 ? result[0].TOTAL_COUNT : 0,
       };
     } catch (error) {
+      console.log(error);
       throw new HttpException('Não foi possível buscar os docs', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
@@ -60,8 +111,8 @@ export class docpcontasnumService {
       return await this.docpcontasnumRepository.findOneOrFail({
         where: { SQE_ID_CODIGO },
       });
-    } catch (error) {      
-      throw new HttpException("Não encontrou nenhum registro", HttpStatus.NOT_FOUND);
+    } catch (error) {
+      throw new HttpException('Não encontrou nenhum registro', HttpStatus.NOT_FOUND);
     }
   }
 }
