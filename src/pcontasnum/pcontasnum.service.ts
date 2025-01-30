7263d51bf434d04dc863349f9371bf5d6810634a
markilha@gmail.com <markilha@gmail.com>
2025-01-30 08:20:33 -0300

    feat: implementar paginação e tratamento de erros no método findAll do PcontasNumService

--- src/pcontasnum/pcontasnum.service.ts
+++ src/pcontasnum/pcontasnum.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { pcontasnumEntity } from '../database/db_oracle/entities/pcontasnum';
 import { FindOptionsWhere, Repository } from 'typeorm';
 import { FindAllParams, pcontasNumDto } from './pcontasnumDto';
+import { getPaginatedQuery } from 'src/util/paginacao/paginaQuery';
 
 @Injectable()
 export class PcontasNumService {
@@ -11,48 +12,85 @@ export class PcontasNumService {
     private pcontasnumRepository: Repository<pcontasnumEntity>,
   ) {}
 
+  // async findAll(params: FindAllParams): Promise<pcontasNumDto[]> {
+  //   try {
+  //     const searchParams: FindOptionsWhere<pcontasNumDto> = {};
+  //     if (params.PCO_ID_CODIGO) {
+  //       searchParams['PCO_ID_CODIGO'] = Number(params.PCO_ID_CODIGO);
+  //     }
+
+  //     if (params.page && params.limit) {
+  //       const page = params.page;
+  //       const limit = params.limit;
+  //       const skip = (page - 1) * limit;
+
+  //       return await this.pcontasnumRepository.find({
+  //         where: searchParams,
+  //         skip,
+  //         take: limit,
+  //       });
+  //     }
+
+  //     return await this.pcontasnumRepository.find({
+  //       where: searchParams,
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw new HttpException(
+  //       'Não foi possível as prestações de conta numerario',
+  //       HttpStatus.INTERNAL_SERVER_ERROR,
+  //     );
+  //   }
+  // }
   async findAll(params: FindAllParams): Promise<pcontasNumDto[]> {
     try {
+      const pageNumber = params.page ?? 1;
+      const pageSize = params.limit ?? 500;
+      const startRow = (pageNumber - 1) * pageSize + 1;
+      const endRow = pageNumber * pageSize;
+
       const searchParams: FindOptionsWhere<pcontasNumDto> = {};
       if (params.PCO_ID_CODIGO) {
         searchParams['PCO_ID_CODIGO'] = Number(params.PCO_ID_CODIGO);
       }
 
-      if (params.page && params.limit) {
-        const page = params.page;
-        const limit = params.limit;
-        const skip = (page - 1) * limit;
+      const queryBuilder = this.pcontasnumRepository
+        .createQueryBuilder('r')
+        .select(['r.PCO_ID_CODIGO as PCO_ID_CODIGO', 'r.RNU_ID_CODIGO as RNU_ID_CODIGO'])
+        .where(searchParams);
 
-        return await this.pcontasnumRepository.find({
-          where: searchParams,
-          skip,
-          take: limit,
-        });
-      }
+      const paginatedQuery = getPaginatedQuery(queryBuilder, startRow, endRow);
+      const parameters = Object.values(queryBuilder.getParameters());
+      const result = await this.pcontasnumRepository.query(paginatedQuery, parameters);
 
-      return await this.pcontasnumRepository.find({
-        where: searchParams,
-      });
+      return result;
     } catch (error) {
       console.log(error);
-      throw new HttpException(
-        'Não foi possível as prestações de conta numerario',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      throw new HttpException('Não foi possível buscar os docs', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
+ 
   async findOne(RNU_ID_CODIGO: number): Promise<pcontasNumDto> {
     try {
-      return await this.pcontasnumRepository.findOne({
-        where: { RNU_ID_CODIGO },
-      });
+      const result = await this.pcontasnumRepository
+        .createQueryBuilder('r')
+        .where('r.RNU_ID_CODIGO = :codigo', { codigo: RNU_ID_CODIGO })
+        .maxExecutionTime(10000)
+        .cache(false)
+        .getOne();
+
+      if (!result) {
+        throw new HttpException('Não encontrou nenhum registro', HttpStatus.NOT_FOUND);
+      }
+      return result;
     } catch (error) {
-      console.log(error);
-      throw new HttpException(
-        'Não foi possível as prestações de conta numerario',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      console.error('Erro ao buscar registro:', error);
+
+      throw new HttpException('Erro ao buscar registro', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
@@ -61,7 +99,7 @@ export class PcontasNumService {
       const pcontasnum = this.pcontasnumRepository.create(data);
       await this.pcontasnumRepository.save(pcontasnum);
       return pcontasnum;
-    } catch (error) {     
+    } catch (error) {
       throw new HttpException(
         'Não foi possível criar a prestação de conta numerario',
         HttpStatus.INTERNAL_SERVER_ERROR,
