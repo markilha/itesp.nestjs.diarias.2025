03cfcb0624aa06d59a2fa771b0487e1b5883773e
markilha@gmail.com <markilha@gmail.com>
2025-07-29 10:12:03 -0300

    refactor: ajustar a lógica de verificação do status em RetonaPrestacaoStatus e RetornaSaquePendentes

--- src/util/variaveis/statusSaquePrestacao.ts
+++ src/util/variaveis/statusSaquePrestacao.ts
@@ -7,13 +7,13 @@ export function RetonaPrestacaoStatus(
   sqedtprest: string,
   sqevlprest: number,
 ) {
-  //  praativo != 'N' &&
   const STATUS =
     [
       SaqueTipoN['35-sim-Viagem'],
       SaqueTipoN['34-sim-Viagem-Reembolso'],
       SaqueTipoN['37-sim-Viagem-Complemento'],
     ].includes(sqeefetivo as SaqueTipoN) &&
+    praativo != 'N' &&
     sqetiposaque === 'N' &&
     (sqedtprest === null || sqevlprest === 0 || sqedtprest === '')
       ? 'Pendente'
@@ -24,15 +24,15 @@ export function RetonaPrestacaoStatus(
   return STATUS;
 }
 export function RetornaSaquePendentes(sqeefetivo: string, sqetiposaque: string, praativo: string) {
-  //&& praativo != 'N'
-  console.log(praativo);
   const STATUS =
     [
       SaqueTipoN['25-não-Viagem'],
       SaqueTipoN['28-não-Viagem-Complemento'],
       SaqueTipoN['32-não-Viagem-c/Lanc-Documentos'],
       SaqueTipoN['48-aguardando-Viagem'],
-    ].includes(sqeefetivo as SaqueTipoN) && sqetiposaque === 'N'
+    ].includes(sqeefetivo as SaqueTipoN) &&
+    sqetiposaque === 'N' &&
+    praativo != 'N'
       ? 'Pendente'
       : 'Realizada';
 
